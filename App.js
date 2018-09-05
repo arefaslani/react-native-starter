6877c0c5635feefe4e6aa8b7b2582d9b3c3424b1
Aref Aslani <arefaslani@gmail.com>
2018-09-05 13:12:44 +0430

    add icons to tab bar

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "react-navigation";
+import Icon from "@expo/vector-icons/FontAwesome";
 
 import FeedPage from './pages/Feed'
 import SettingsPage from './pages/Settings'
@@ -7,4 +8,20 @@ import SettingsPage from './pages/Settings'
 export default createBottomTabNavigator({
   Feed: FeedPage,
   Settings: SettingsPage
+}, {
+  navigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, tintColor }) => {
+      const { routeName } = navigation.state;
+      switch (routeName) {
+        case 'Feed':
+          return <Icon name="home" size={25} color={tintColor} />;
+          break;
+        case 'Settings':
+          return <Icon name="rocket" size={25} color={tintColor} />;
+          break;
+        default:
+          break;
+      }
+    }
+  })
 });
