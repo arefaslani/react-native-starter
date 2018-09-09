ab9473c97475e6b41cfc5b8ded98ec74549bb264
Aref Aslani <arefaslani@gmail.com>
2018-09-09 08:53:27 +0430

    remove assets

--- Routes.js
+++ Routes.js
@@ -9,10 +9,6 @@ import Icon from "@expo/vector-icons/FontAwesome";
 import FeedPage from "pages/Feed";
 import PostPage from "pages/Post";
 import SettingsPage from "pages/Settings";
-import tabChangeSound from "assets/sounds/tab.m4a";
-/* eslint-disable no-undef */
-const soundObject = new Expo.Audio.Sound();
-/* eslint-enable no-undef */
 
 const FeedStack = createStackNavigator({
   Feed: {
@@ -45,37 +41,22 @@ const SettingsTabIcon = ({ tintColor }) => (
   <Icon name="sliders" size={25} color={tintColor} />
 );
 
-const Routes = createBottomTabNavigator(
-  {
-    Feed: {
-      screen: FeedStack,
-      navigationOptions: {
-        title: "Home",
-        tabBarIcon: HomeTabIcon
-      }
-    },
-    Settings: {
-      screen: SettingsStack,
-      navigationOptions: {
-        title: "Settings",
-        tabBarIcon: SettingsTabIcon
-      }
+const Routes = createBottomTabNavigator({
+  Feed: {
+    screen: FeedStack,
+    navigationOptions: {
+      title: "Home",
+      tabBarIcon: HomeTabIcon
     }
   },
-  {
+  Settings: {
+    screen: SettingsStack,
     navigationOptions: {
-      tabBarOnPress: async ({ defaultHandler }) => {
-        try {
-          await soundObject.loadAsync(tabChangeSound);
-        } catch (error) {
-          // do nothing
-        }
-        await soundObject.replayAsync();
-        defaultHandler();
-      }
+      title: "Settings",
+      tabBarIcon: SettingsTabIcon
     }
   }
-);
+});
 
 HomeTabIcon.propTypes = {
   tintColor: PropTypes.string.isRequired
