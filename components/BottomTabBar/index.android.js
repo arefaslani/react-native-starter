00db9aa0200c3a6164115fdfab73e458ee60b02f
Aref Aslani <arefaslani@gmail.com>
2018-09-12 15:17:34 +0430

    add sounds to android bottom tab bar

--- components/BottomTabBar/index.android.js
+++ components/BottomTabBar/index.android.js
@@ -1,5 +1,13 @@
 import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
 
 import Routes from "Routes";
+import play from "utilities/playSound";
 
-export default createMaterialBottomTabNavigator(Routes);
+export default createMaterialBottomTabNavigator(Routes, {
+  navigationOptions: {
+    tabBarOnPress: ({ defaultHandler }) => {
+      play("tab");
+      defaultHandler();
+    }
+  }
+});
