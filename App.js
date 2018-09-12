4eee79325a270bdafee5e004e2ae0d113562104b
Aref Aslani <arefaslani@gmail.com>
2018-09-12 17:26:09 +0430

    force portrait screen

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Provider } from "react-redux";
+import Expo from "expo";
 
 import store from "store/store";
 
 import BottomTabBar from "components/BottomTabBar"; // eslint-disable-line
+
+Expo.ScreenOrientation.allow(Expo.ScreenOrientation.Orientation.PORTRAIT);
 
 export default () => (
   <Provider store={store}>
